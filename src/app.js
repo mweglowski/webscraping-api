8df78b1c9845b64611c99fcf4d9c6e274d18bacf
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-06-06 10:04:17 +0200

    testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const morgan = require("morgan");
 const helmet = require("helmet");
-const cors = require("cors");
+const cors = require("cors")
 const axios = require("axios");
 const cheerio = require("cheerio");
 
@@ -12,35 +11,25 @@ const api = require("./api");
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(helmet());
-
-// Adjust allowed origins based on your needs
-app.use(cors({
-  origin: ["http://localhost:3000", "https://webscraper-api.vercel.app"],
-  methods: ["GET"],
-  allowedHeaders: ["Content-Type"],
-}));
+app.use(cors())
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("http://deslux.pl/");
+    const response = await axios.get("https://rs-real-estate.nieruchomosci-online.pl/");
     const html = response.data;
     const $ = cheerio.load(html);
 
     const data = [];
 
-    $("li.blog-grid-style").each((index, element) => {
-      const imageSrc = $(element).find("div.post-media img").attr("src");
-      const title = $(element).find("h2.post-title a").text();
-      const link = $(element).find("h2.post-title a").attr("href");
+    $("ul.thumb-slider li").each((index, element) => {
+      const imageSrc = $(element).find("img").attr("src");
+      // const title = $(element).find("h2.post-title a").text();
+      // const link = $(element).find("h2.post-title a").attr("href");
 
-      // Update image URL to use the proxy endpoint
-      const proxyImageUrl = `${req.protocol}://${req.get('host')}/proxy-image?url=${encodeURIComponent(imageSrc)}`;
-
-      data.push({ imageSrc: proxyImageUrl, title, link });
+      data.push({ imageSrc });
     });
 
     res.json({
@@ -50,30 +39,6 @@ app.get("/", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error occurred during web scraping" });
-  }
-});
-
-// Proxy route for HTTP images
-app.get("/proxy-image", async (req, res) => {
-  try {
-    const imageUrl = req.query.url;
-    if (!imageUrl) {
-      return res.status(400).json({ message: "Image URL is required" });
-    }
-
-    const response = await axios({
-      url: imageUrl,
-      responseType: "stream",
-    });
-
-    res.setHeader("Content-Type", response.headers["content-type"]);
-    res.setHeader("Access-Control-Allow-Origin", "*"); // Allow CORS for all origins
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    response.data.pipe(res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error occurred during image proxying" });
   }
 });
 
