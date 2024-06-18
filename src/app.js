ff730f7dffa0d4e973ae7985c5e0866be403b58c
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-06-18 09:56:07 +0200

    f

--- src/app.js
+++ src/app.js
@@ -17,64 +17,66 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  try {
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    const targetUrl = "http://deslux.pl/";
-    
-    const response = await axios.get(`${proxyUrl}${targetUrl}`, {
-      headers: {
-        'origin': 'https://ladyfinanse.vercel.app',  // Use your development or production domain
-        'x-requested-with': 'XMLHttpRequest',
-      }
-    });
-    
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    const data = [];
-
-    $("div.post-media").each((index, element) => {
-      const imageSrc = $(element).find("img").attr("src");
-      data.push({ imageSrc });
-    });
-
-    res.status(200).json({
-      message: "Scraped data",
-      data,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error occurred during web scraping", error: error.message });
-  }
   // try {
-    // const response = await axios.get("https://rynekpierwotny.pl/s/nowe-mieszkania-i-domy-warszawa/");
-  //   const response = await axios.get("http://deslux.pl/");
+  //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  //   const targetUrl = "http://deslux.pl/";
+
+  //   const response = await axios.get(`${proxyUrl}${targetUrl}`, {
+  //     headers: {
+  //       'origin': 'https://ladyfinanse.vercel.app',  // Use your development or production domain
+  //       'x-requested-with': 'XMLHttpRequest',
+  //     }
+  //   });
+
   //   const html = response.data;
   //   const $ = cheerio.load(html);
 
   //   const data = [];
-  //   console.log("siema");
 
-  //   // $("div.rp-trzvq1").each((index, element) => {
-  //   //   const imageSrc = $(element).find("img").attr("src");
-  //   // const title = $(element).find("h2.post-title a").text();
-  //   // const link = $(element).find("h2.post-title a").attr("href");
   //   $("div.post-media").each((index, element) => {
   //     const imageSrc = $(element).find("img").attr("src");
-
   //     data.push({ imageSrc });
   //   });
 
-  //   // });
-
-  //   res.json({
+  //   res.status(200).json({
   //     message: "Scraped data",
   //     data,
   //   });
   // } catch (error) {
   //   console.error(error);
-  //   res.status(500).json({ message: "Error occurred during web scraping" });
+  //   res.status(500).json({ message: "Error occurred during web scraping", error: error.message });
   // }
+  try {
+    const response = await axios.get(
+      "https://rynekpierwotny.pl/s/nowe-mieszkania-i-domy-warszawa/"
+    );
+    // const response = await axios.get("http://deslux.pl/");
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const data = [];
+    console.log("siema");
+
+    $("div.rp-trzvq1").each((index, element) => {
+      const imageSrc = $(element).find("img").attr("src");
+      const title = $(element).find("h2.post-title a").text();
+      const link = $(element).find("h2.post-title a").attr("href");
+      // $("div.post-media").each((index, element) => {
+      //   const imageSrc = $(element).find("img").attr("src");
+
+      data.push({ imageSrc });
+    });
+
+    // });
+
+    res.json({
+      message: "Scraped data",
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occurred during web scraping" });
+  }
 });
 
 app.use("/api", api);
